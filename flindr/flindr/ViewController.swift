f4fd6619a8c91a197a2b8b2c74425c2f52bfaa71
rymotion1@me.com <rymotion1@me.com>
2016-04-02 19:10:41 -0700

    started coding

---
+++ flindr/flindr/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  flindr
+//
+//  Created by Ryan Paglinawan on 4/2/16.
+//  Copyright © 2016 RyanPaglinawan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func didSwipeLeft(){
+        // reject movie
+        nextMovie();
+    }
+    
+    func didSwipeRight(){
+        // accept and save movie
+        
+        nextMovie();
+    }
+    
+    func nextMovie(){
+        // this will get the next movie from the list on the database API
+        
+    }
+
+}
+
